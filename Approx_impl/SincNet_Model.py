0daff5f5d75c02d9918cc4b4386a88d5c917c8a8
bALAJi-aDITHYa <my.github.balaji@gmail.com>
2021-06-28 23:47:21 +0530

    model with cordic + simdive + approx. log_comp

--- Approx_impl/SincNet_Model.py
+++ Approx_impl/SincNet_Model.py
@@ -12,10 +12,72 @@ from torch.autograd import Variable
 import math
 from torch.utils.cpp_extension import load
 
+import csv
+import pandas as pd
+filename = "ref_div.csv"
+rows = []
+
+with open(filename,'r') as file:
+	rows = list(csv.reader(file))
+
+	for row in rows[1:]:
+		row[0], row[1], row[2] = int(row[0]), int(row[1]), int(row[2])
+
+lookup_div = np.zeros((256,256))
+for row in rows[1:]:
+	lookup_div[row[0]][row[1]] = row[2]
+
+def SimDive(a, b):
+	out = torch.zeros(a.shape)
+	sign=0
+
+	for i in range(a.size(0)):
+		t1 = int((a[i]*100).round())
+		t2 = b[i]
+		if(t2>25):
+			t2 = t2/4
+			p = 1/4
+		elif (t2>2 and t2<=25):
+			t2 = t2
+			p = 1
+		else:
+			t2 = t2*8
+			p= 8
+		t2 = int(t2.round())
+
+
+		if(t1>255) : t1 =255
+		if(t1<-255): t1 =-255
+		if(t2>255) : t2 =255
+		if(t2<-255): t2 =-255
+
+		if((t1>0 and t2>0) or (t1<0 and t2<0)):
+			t1=abs(t1)
+			t2=abs(t2)
+			sign=1
+		else:
+			t1=abs(t1)
+			t2=abs(t2)
+			sign=-1
+
+
+		out[i] = sign*(lookup_div[t1][t2] * p)/100.0
+
+	return out
+
+
 custom_sine = load(name="custom_sine", sources=["c_sine.cpp"], verbose=True)
+custom_log = load(name="custom_log", sources=["c_log.cpp"], verbose=True)
+
+def approx_Log(inp):
+	t_inp = inp.to(torch.device('cpu'))
+	out = torch.Tensor(list(inp.shape))
+	out = custom_log.Log(t_inp)
+	out = out.cuda()
+
+	return out
 
 def approx_sine(inp):
-	# n = inp.size[0]
 	t_inp = inp.to(torch.device('cpu'))
 	out = torch.Tensor(list(inp.shape))
 	out = custom_sine.sine(t_inp)
@@ -33,19 +95,11 @@ def flip(x, dim):
 	return x.view(xsize)
 
 def sinc(band,t_right):
-	# temp = torch.abs(2*math.pi*band*t_right)
-	# print('max = {}'.format(torch.max(temp)))
-	# print('min = {}'.format(torch.min(temp)))
-	# print('mean = {}'.format(torch.mean(temp)))
-	# p = 2*math.pi*band*t_right
-	# print(p.shape)
-
-	# y_right= torch.sin(2*math.pi*band*t_right)/(2*math.pi*band*t_right)
-	y_right = approx_sine(2*math.pi*band*t_right)/(2*math.pi*band*t_right)
-	y_left= flip(y_right,0)
+	y_right = approx_sine((2*math.pi*band*t_right)%(2*math.pi))
+	y_right = SimDive(y_right, 2*math.pi*band*t_right).cuda()
+	y_left= flip(y_right,0).cuda()
 
 	y=torch.cat([y_left,Variable(torch.ones(1)).cuda(),y_right])
-	# print(y.shape)
 
 	return y
 
@@ -59,9 +113,7 @@ class log_compression(nn.Module):
 	
 	def forward(self, x):
 		#log - compression (used to scale the range of values from 0-n)
-		x = torch.add(torch.abs(x),1)
-		x = torch.log2(x)	#torch.log2()/ torch.log()
-
+		x = approx_Log(x)
 		x = self.bnorm(x)
 		x = self.avgpool(x)
 		return x
@@ -82,7 +134,6 @@ class DS_conv_op_pad(nn.Sequential):
 			nn.ReLU(inplace=True),
 			nn.BatchNorm1d(output_channels),
 			nn.AvgPool1d(kernel_size=2, stride=None, padding=1)
-			# nn.AvgPool1d(2)
 			)
 
 class DS_conv_op(nn.Sequential):
@@ -100,7 +151,6 @@ class DS_conv_op(nn.Sequential):
 			nn.Conv1d(input_channels, output_channels, kernel_size =1, stride=stride), #Pointwise
 			nn.ReLU(inplace=True),
 			nn.BatchNorm1d(output_channels),
-			# nn.AvgPool1d(kernel_size=2, stride=None, padding=1)
 			nn.AvgPool1d(2)
 			)
 
@@ -129,13 +179,15 @@ class DSConv_block(nn.Module):
 
 	def forward(self, x):
 		x = self.ds_layers(x)
-		# print("In DS_conv_op")
-		# print(x.shape)
 		x = self.drop(x)
 
 		return x
 
-
+# ------------------------------------------------------------
+# Sinc conv block creates 40 filters, each of length 101, which is basically a sinc filter
+# The learnable parameters are filt-b1 and filt-band, hence in total, this layer has 80 learned params
+# These filters are convolved to input raw audio data to extract features
+# ------------------------------------------------------------
 
 class sinc_conv(nn.Module):
 	def __init__(self, n_filt=40, filt_dim=101,fs=16000):
@@ -173,7 +225,6 @@ class sinc_conv(nn.Module):
 
 		n=torch.linspace(0, N, steps=N)
 
-		# Filter window (hamming)
 		window=0.54-0.46*torch.cos(2*math.pi*n/N);
 		window=Variable(window.float().cuda())
 
@@ -188,14 +239,16 @@ class sinc_conv(nn.Module):
 
 		    filters[i,:]=band_pass.cuda()*window
 
-		# print(filters.view(40,1,101).shape)
-
 		out = F.conv1d(x, filters.view(self.n_filt,1,self.filt_dim), stride=8)
-		# print("out in sinc")
-		# print(out.shape)
 
 		return out
 
+
+# --------------------------------------------------
+# SincNet class definition beins here
+# Major blocks present are - SincConv, log_compression, DSConv_blocks (Depth-wise)
+# Classifier used - Log Softmax
+# ---------------------------------------------------
 
 class SincNet(nn.Module):
 	def __init__(
@@ -214,18 +267,20 @@ class SincNet(nn.Module):
 			[160, 9 ,1,4],
 			[160, 9 ,1,5],					
 		]
-		n_filt = 40 #Output channels from SincConv
-		filt_dim = 101 #Kernel length for sincconv-layer
-		fs = 16000 #sampling rate of the audio file
+
+		#Output channels from SincConv
+		n_filt = 40 
+
+		#Kernel length for sincconv-layer
+		filt_dim = 101 
+
+		#sampling rate of the audio file
+		fs = 16000 
 	
 		sincconv_block: List[nn.Module] = [sinc_conv(n_filt, filt_dim, fs)]
 		sincconv_block.append(log_compression(n_filt))
 
-		self.sincconv_block = nn.Sequential(*sincconv_block)
-		
-		
-		# ds_features: List[nn.Module] = [DSConv_block(n_filt, output_channels=160, kernel_size=25, stride=2)]
-		
+		self.sincconv_block = nn.Sequential(*sincconv_block)		
 
 		ds_features: List[nn.Module] = []
 		input_channels = n_filt
@@ -245,11 +300,9 @@ class SincNet(nn.Module):
 
 	def forward(self, x: Tensor)-> Tensor:
 		x = self.sincconv_block(x)
-		# print(x.shape)
 		x = self.ds_features(x)
 		x = self.global_avg(x)
 		x = torch.flatten(x,1)
-		# print(x.shape)		
 		x = self.classifier(x)
 
 		return x
